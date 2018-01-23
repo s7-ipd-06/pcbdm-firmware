d6020b37346891fce291d5f4b921ab513b43fbdb
seymar <seymar@users.noreply.github.com>
2018-01-23 15:02:52 +0100

    Initial commit motor drivers

---
+++ MotorTimers.ino
@@ -0,0 +1,77 @@
+#include <SPI.h>
+#include <AMIS30543.h>
+
+#define DIRPINX 8
+#define PULSEPINX 9
+
+#define DIRPINY 23
+#define PULSEPINY 22
+AMIS30543 stepperx;
+AMIS30543 steppery;
+void setup(){
+  
+  
+  setTimers(); //Timers tab
+  
+
+Serial.begin(9600);
+  SPI.begin();
+  stepperx.init(SS);
+  steppery.init(24);
+  delay(1);
+
+  stepperx.resetSettings();
+  stepperx.setCurrentMilliamps(1260);
+  stepperx.setStepMode(8);
+  stepperx.enableDriver();
+  
+  steppery.resetSettings();
+  steppery.setCurrentMilliamps(1260);
+  steppery.setStepMode(8);
+  steppery.enableDriver();
+
+
+}//end setup
+
+
+//int qw = 0;
+boolean as = true;
+void loop(){
+  while(Serial.available()) {
+    //qw++;
+      String a= Serial.readString();// read the incoming data as string
+      if(a.charAt(0) == 'x'){
+        as = true;
+      }else{
+      if(a.charAt(0) == 'y'){
+        as = false;
+      }else{
+        if(a.charAt(0) == 'c'){
+        
+      }else{
+      //int i = a.toInt();
+      long i = atol(a.c_str());
+      if(i<-1000000 || i>1000000){
+         Serial.println("Number not allowed"); 
+      }else{
+        if(as){
+          setspeedX(i);
+          Serial.print("Setting X-Speed: ");
+          Serial.println(i);
+        }else{
+          setspeedY(i);
+          Serial.print("Setting Y-Speed: ");
+          Serial.println(i);
+        }
+        
+        
+        //updateFPGA((byte)i);
+        //Serial.print("New value: ");
+        //Serial.println(i);
+      }}}}
+  }
+  //Serial.print(qw);
+  //getXPos();
+ 
+  
+}
--- Timers.ino
+++ Timers.ino
@@ -1,13 +1,26 @@
 void setTimers(){
  cli();//stop interrupts
-  //Don't use timer0, It is reserves for delay() and millis() and interferes with the serial read.
 
-  //set timer1 interrupt at 1kHz
+//set timer0 interrupt at 2kHz
+/*
+  TCCR0A = 0;// set entire TCCR2A register to 0
+  TCCR0B = 0;// same for TCCR2B
+  TCNT0  = 0;//initialize counter value to 0
+  // set compare match register for 2khz increments
+  OCR0A = 19;// = (16*10^6) / (2000*64) - 1 (must be <256)
+  // turn on CTC mode
+  TCCR0A |= (1 << WGM01);
+  // Set CS01 and CS00 bits for 64 prescaler
+  TCCR0B |= (1 << CS01) ;//| (1 << CS00);   
+  // enable timer compare interrupt
+  TIMSK0 |= (1 << OCIE0A);
+*/
+//set timer1 interrupt at 1Hz
   TCCR1A = 0;// set entire TCCR1A register to 0
   TCCR1B = 0;// same for TCCR1B
   TCNT1  = 0;//initialize counter value to 0
-  
-  OCR1A = 15;// 
+  // set compare match register for 1hz increments
+  OCR1A = 15;// = (16*10^6) / (1*1024) - 1 (must be <65536)
   // turn on CTC mode
   TCCR1B |= (1 << WGM12);
   // Set CS12 and CS10 bits for 1024 prescaler
@@ -15,7 +28,7 @@ void setTimers(){
   // enable timer compare interrupt
   TIMSK1 |= (1 << OCIE1A);
   
-//set timer2 interrupt at 20kHz
+//set timer2 interrupt at 8kHz
   TCCR2A = 0;// set entire TCCR2A register to 0
   TCCR2B = 0;// same for TCCR2B
   TCNT2  = 0;//initialize counter value to 0
@@ -27,7 +40,7 @@ void setTimers(){
   TCCR2B |= (1 << CS21);   
   // enable timer compare interrupt
   TIMSK2 |= (1 << OCIE2A);
-  //set timer3 at 100kz
+  
   TCCR3A = 0;// set entire TCCR1A register to 0
   TCCR3B = 0;// same for TCCR1B
   TCNT3  = 0;//initialize counter value to 0
@@ -42,212 +55,89 @@ void setTimers(){
 sei();//allow interrupts 
   
 }
-volatile long cmp_X = 50000;
-volatile long cmp_Y = 50000;
-volatile long cmp_Z = 50000;
+volatile long cmpx = 50000;
+volatile long cmpy = 50000;
 
-volatile long pc_X;
-volatile boolean state_X=false;
-volatile boolean stopped_X = true;
+volatile long pcx;
+volatile boolean statex=false;
+volatile boolean stoppedx = true;
 
-volatile long pc_Y;
-volatile boolean state_Y=false;
-volatile boolean stopped_Y = true;
+volatile long pcy;
+volatile boolean statey=false;
+volatile boolean stoppedy = true;
 
-volatile long pc_Z;
-volatile boolean state_Z=false;
-volatile boolean stopped_Z = true;
-//volatile long countx;
-//volatile long county;
+
+volatile long countx;
+volatile long county;
 
 
 ISR(TIMER3_COMPA_vect){//100K 
- pc_X++;
- pc_Y++;
- pc_Z++;
+ pcx++;
+ pcy++;
 }
 
 
 
-volatile long oldspeed_X = 0;
-volatile long desspeed_X = 0;
+volatile long oldspeedx = 0;
+volatile long desspeedx = 0;
 
-volatile long oldspeed_Y = 0;
-volatile long desspeed_Y = 0;
+volatile long oldspeedy = 0;
+volatile long desspeedy = 0;
 
-volatile long oldspeed_Z = 0;
-volatile long desspeed_Z = 0;
-
-void setSpeedX(int freq){
-  if(LOCKED){
-    freq =0;
-  }
-  desspeed_X = freq;
+void setspeedX(int freq){
+  desspeedx = freq;
 }
-void setSpeedY(int freq){
-  if(LOCKED){
-    freq =0;
-  }
-  desspeed_Y = freq;
-}
-void setSpeedZ(int freq){
-  desspeed_Z = freq;
-}
-long getSpeedX(){
-  return desspeed_X;
-}
-long getSpeedY(){
-  return desspeed_Y;
-}
-long getSpeedZ(){
-  return desspeed_Z;
+void setspeedY(int freq){
+  desspeedy = freq;
 }
 const int accel = 20;
-long GETES_ZERO_X_STATE(){
-  return desspeed_X;
-}
-volatile int debug_state=0;
-ISR(TIMER1_COMPA_vect){
-  endSwitchRead();
-  controlLoop();
-  if(!stable){
-    stableCheck();
-  }
-  
-  endSwitchRead();
-  if(desspeed_X == oldspeed_X){
-    //No update required
-  }else{
-    if(desspeed_X > 0){  //forward
-      if(ES_MAX_X_STATE){  //if endswitch MAX is hit, do not allow forward motion
-        forcespeedX(0);
-        oldspeed_X=0;
-      }else{    //Forward motion is allowed
-        if(desspeed_X>oldspeed_X+accel){  //check if the speed change will exceed the max acceleration
-          if(oldspeed_X<0){  //Instant stop is allowed, so if the current motion is backward, stop and accel forward.
-            forcespeedX(accel);
-            oldspeed_X=accel;
-          }else{  //accell up.
-            forcespeedX(oldspeed_X+accel);
-            oldspeed_X+=accel;
-          }
-        }else{
-           forcespeedX(desspeed_X);
-           oldspeed_X = desspeed_X;
-        }
-      }
-      }else{    //backward
-        if(ES_ZERO_X_STATE){
-          forcespeedX(0);
-          oldspeed_X=0;
-        }else{
-          if(desspeed_X<oldspeed_X-accel&&desspeed_X!=0){
-            if(oldspeed_X>0){
-              forcespeedX(-accel);
-              oldspeed_X=-accel;
-            }else{
-              forcespeedX(oldspeed_X-accel);
-              oldspeed_X-=accel;
-            }
-          }else{
-             forcespeedX(desspeed_X);
-             oldspeed_X = desspeed_X;
-          }
-        }
-      }
+
+ISR(TIMER1_COMPA_vect){//timer1 interrupt 1Hz toggles pin 13 (LED)
+//generates pulse wave of frequency 1Hz/2 = 0.5kHz (takes two cycles for full wave- toggle high then toggle low)
+  if(desspeedx == oldspeedx){
     
-  }// END X
-  
-  
-  if(desspeed_Y == oldspeed_Y){
-    debug_state = 6;
   }else{
-    if(desspeed_Y > 0){
-      if(ES_MAX_Y_STATE){
-        debug_state=1;
-        forcespeedY(0);
-        oldspeed_Y=0;
-      }else{
-        if(desspeed_Y>oldspeed_Y+accel){
-          if(oldspeed_Y<0){
-            debug_state=2;
-            forcespeedY(accel);
-            oldspeed_Y=accel;
-          }else{
-            forcespeedY(oldspeed_Y+accel);
-            oldspeed_Y+=accel;
-          }
-        }else{
-          debug_state=0;
-           forcespeedY(desspeed_Y);
-           oldspeed_Y = desspeed_Y;
-        }
-      }
+    
+  if(desspeedx > 0){
+    if(desspeedx>oldspeedx+accel){
+      forcespeedx(oldspeedx+accel);
+      oldspeedx+=accel;
     }else{
-      if(ES_ZERO_Y_STATE){
-        forcespeedY(0);
-        oldspeed_Y=0;      
-      }else{
-        if(desspeed_Y<oldspeed_Y-accel&&desspeed_Y!=0){
-          if(oldspeed_Y>0){
-            forcespeedY(-accel);
-            oldspeed_Y=-accel;
-          }else{
-            forcespeedY(oldspeed_Y-accel);
-            oldspeed_Y-=accel;
-          }
-        }else{
-          forcespeedY(desspeed_Y);
-          oldspeed_Y = desspeed_Y;
-        }
-      }
+       forcespeedx(desspeedx);
+       oldspeedx = desspeedx;
     }
-  }
-  
-  
-  if(desspeed_Z == oldspeed_Z){
-    //No update required
   }else{
-    if(desspeed_Z > 0){
-      if(ES_MAX_Z_STATE){
-        forcespeedZ(0);
-        oldspeed_Z=0;
-      }else{
-        if(desspeed_Z>oldspeed_Z+accel){
-          if(oldspeed_Z<0){
-            forcespeedZ(accel);
-            oldspeed_Z=accel;
-          }else{
-            forcespeedZ(oldspeed_Z+accel);
-            oldspeed_Z+=accel;
-          }
-        }else{
-           forcespeedZ(desspeed_Z);
-           oldspeed_Z = desspeed_Z;
-        }
-      }
+    if(desspeedx<oldspeedx-accel&&desspeedx!=0){
+      forcespeedx(oldspeedx-accel);
+      oldspeedx-=accel;
     }else{
-      if(ES_ZERO_Z_STATE){
-        forcespeedZ(0);
-        oldspeed_Z=0;      
-      }else{
-        if(desspeed_Z<oldspeed_Z-accel&&desspeed_Z!=0){
-          if(oldspeed_Z>0){
-            forcespeedZ(-accel);
-            oldspeed_Z=-accel;
-          }else{
-            forcespeedZ(oldspeed_Z-accel);
-            oldspeed_Z-=accel;
-          }
-        }else{
-          forcespeedZ(desspeed_Z);
-          oldspeed_Z = desspeed_Z;
-        }
-      }
+       forcespeedx(desspeedx);
+       oldspeedx = desspeedx;
     }
-  }
+  }}
   
   
+  if(desspeedy == oldspeedy){
+    
+  }else{
+    
+  if(desspeedy > 0){
+    if(desspeedy>oldspeedy+accel){
+      forcespeedy(oldspeedy+accel);
+      oldspeedy+=accel;
+    }else{
+       forcespeedy(desspeedy);
+       oldspeedy = desspeedy;
+    }
+  }else{
+    if(desspeedy<oldspeedy-accel&&desspeedy!=0){
+      forcespeedy(oldspeedy-accel);
+      oldspeedy-=accel;
+    }else{
+       forcespeedy(desspeedy);
+       oldspeedy = desspeedy;
+    }
+  }}
   /*
   Serial.print("X: ");
   Serial.print(countx);
@@ -262,83 +152,48 @@ ISR(TIMER1_COMPA_vect){
   county = 0;
   */
 }
-void showinfo(){
   
- Serial.print(desspeed_Z);
- Serial.print("\t");
- Serial.print(stopped_Z);
- Serial.print("\t");
- Serial.print(ES_ZERO_Z_STATE);
- Serial.print("\t");
- Serial.print(pc_Z);
- Serial.print("\t");
- Serial.println(cmp_Z);
-}
-
-void showAllow(){
- Serial.print(ES_MAX_X_STATE?"\tForward Denied":"Forward Allowed"); 
- Serial.print(ES_ZERO_X_STATE?"\tBackward Denied":"Backward Allowed"); 
- Serial.print(ES_MAX_Y_STATE?"\tForward Denied":"Forward Allowed"); 
- Serial.print(ES_ZERO_Y_STATE?"\tBackward Denied":"Backward Allowed");
- Serial.print(ES_MAX_Z_STATE?"\tDOWN Denied":"DOWN Allowed"); 
- Serial.println(ES_ZERO_Z_STATE?"\tUP Denied":"UP Allowed");
- 
-
-}
-volatile boolean DIR_Z = HIGH;
-ISR(TIMER2_COMPA_vect){//20K PULSER
+ISR(TIMER2_COMPA_vect){//20K
   
-  if(stopped_X || LOCKED){
-    pc_X=0;
+  if(stoppedx){
+    pcx=0;
   }else{
-    if(pc_X>=cmp_X){
-      pc_X-=cmp_X;
-      
-      state_X = !state_X;
-      digitalWrite(PULSEPIN_X,state_X);
+    if(pcx>=cmpx){
+      pcx-=cmpx;
+      if(statex){
+        countx++;
+      }
+      statex = !statex;
+      digitalWrite(PULSEPINX,statex);
     }
   } 
-  if(stopped_Y || LOCKED){
-    pc_Y=0;
-    
+  if(stoppedy){
+    pcy=0;
+    return;
   }else{
-    if(pc_Y>=cmp_Y){
-      pc_Y-=cmp_Y;
-      
-      state_Y = !state_Y;
-      digitalWrite(PULSEPIN_Y,state_Y);
-    }
-  }
- if(stopped_Z){
-    pc_Z=0;
-    
-  }else{
-    if(pc_Z>=cmp_Z){
-      pc_Z-=cmp_Z;
-      
-      state_Z = !state_Z;
-      if(state_Z){
-        encZ(DIR_Z);
+    if(pcy>=cmpy){
+      pcy-=cmpy;
+      if(statey){
+        county++;
       }
-      digitalWrite(PULSEPIN_Z,state_Z);
+      statey = !statey;
+      digitalWrite(PULSEPINY,statey);
     }
   } 
 }
 const float base = 100000/2;
-void forcespeedX(int freq){
-  oldspeed_X = freq;
-  pc_X = 0;
+void forcespeedx(int freq){
+  pcx = 0;
   if(freq == 0){
-     stopped_X = true; 
+     stoppedx = true; 
   }else{
-    stopped_X = false;
+    stoppedx = false;
   }
   if(freq<0){
     freq = -freq;
-    digitalWrite(DIRPIN_X,HIGH);
-    
+    digitalWrite(DIRPINX,HIGH);
   }else{
-    digitalWrite(DIRPIN_X,LOW);
+    digitalWrite(DIRPINX,LOW);
   }
   
   float f = (float)freq;
@@ -349,23 +204,22 @@ void forcespeedX(int freq){
   if(d > 500000){
     d = 50000;
   }
-  cmp_X = d;
+  cmpx = d;
  // Serial.print("New X comp value: ");
  // Serial.println(d);
 }
-void forcespeedY(int freq){
-  oldspeed_Y = freq;
-  pc_Y = 0;
+void forcespeedy(int freq){
+  pcy = 0;
   if(freq == 0){
-     stopped_Y = true; 
+     stoppedy = true; 
   }else{
-    stopped_Y = false;
+    stoppedy = false;
   }
   if(freq<0){
     freq = -freq;
-    digitalWrite(DIRPIN_Y,HIGH);
+    digitalWrite(DIRPINY,HIGH);
   }else{
-    digitalWrite(DIRPIN_Y,LOW);
+    digitalWrite(DIRPINY,LOW);
   }
   
   float f = (float)freq;
@@ -376,38 +230,7 @@ void forcespeedY(int freq){
   if(d > 500000){
     d = 50000;
   }
-  cmp_Y = d;
-  //Serial.print("New Y comp value: ");
-  //Serial.println(d);
-}
-
-void forcespeedZ(int freq){
-  oldspeed_Z = freq;
-  pc_Z = 0;
-  if(freq == 0){
-     stopped_Z = true; 
-  }else{
-    stopped_Z = false;
-  }
-  if(freq<0){
-    freq = -freq;
-    digitalWrite(DIRPIN_Z,HIGH);
-    DIR_Z = HIGH;
-    
-  }else{
-    digitalWrite(DIRPIN_Z,LOW);
-    DIR_Z = LOW;
-  }
-  
-  float f = (float)freq;
-  int d = base/freq;
-  if(d<5){
-    d=5;
-  }
-  if(d > 500000){
-    d = 50000;
-  }
-  cmp_Z = d;
+  cmpy = d;
   //Serial.print("New Y comp value: ");
   //Serial.println(d);
 }
